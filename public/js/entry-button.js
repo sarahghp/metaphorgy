58a5c66f69e62bd88177c3fdeefd0e7c5b734c79
Sarah Groff-Palermo <sarah.groff.palermo@gmail.com>
2013-11-13 15:29:42 -0800

    Css transitions, broken opacity

--- public/js/entry-button.js
+++ public/js/entry-button.js
@@ -2,9 +2,6 @@ $(document).ready(function(){
   $(".btn-entry").click(
     function(){
       $(".btn-entry").toggleClass("btn-success3d");
-      $("#question").fadeToggle("5000", function(){
-        $("#answer").fadeToggle("5000");
+      $("#question, #answer").toggleClass("hidden");
       });
-    }
-    );
 })
