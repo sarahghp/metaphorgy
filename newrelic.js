02398aa376a86da1b38e067e93cd14d7ecd3f08f
Jack Lawson <ajacksified@gmail.com>
2013-11-18 16:50:03 -0800

    add newrelic

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name : ['Metaphorgy'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key : '8d085bb5f407e44a94831246c070e0a4c3de061c',
+  logging : {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level : 'trace'
+  }
+};
